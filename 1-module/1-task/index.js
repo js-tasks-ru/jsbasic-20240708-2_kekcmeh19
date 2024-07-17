286b877548a58bbd8696053d65a80812225a6842
mdgeer <mdgeer25@gmail.com>
2024-07-17 18:39:27 +0200

    task 1.1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
   // ваш код...
+  if( n === 0 || n === 1) {
+    return 1;
+  } else {
+    let fact = n;
+
+    for(let i = 1; i < n; i++) {
+      fact = fact * (n - i);
+    }
+  
+    return fact;
+  }
 }
