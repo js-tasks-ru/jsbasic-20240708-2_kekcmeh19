56439106fa30cbb6fb741d41723d016a094b344d
mdgeer <mdgeer25@gmail.com>
2024-07-17 19:35:29 +0200

    task 1.3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
   // ваш код...
+  if( str === '') {
+    return str;
+  } else {
+    let firstChar = str.slice(0, 1).toUpperCase();
+    let strWithoutFirstChar = str.slice(1);
+    return( firstChar + strWithoutFirstChar)
+  }
 }
