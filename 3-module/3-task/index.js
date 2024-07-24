798dd0ad49a485e34375d20873de4280ed1afc93
mdgeer <mdgeer25@gmail.com>
2024-07-24 21:32:26 +0200

    task 3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
   // ваш код...
+  let words = str.split('-');
+  let camelWords = [words[0]];
+
+  for (let i = 1; i < words.length; i++) {
+    let camelWord = words[i].slice(0, 1).toUpperCase() + words[i].slice(1);
+    camelWords.push(camelWord);
+  }
+
+  return (camelWords.join(''));
 }
