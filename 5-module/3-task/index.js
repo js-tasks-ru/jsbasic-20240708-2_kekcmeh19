e2a27fdbc2e9619e4dae4de61454a4e3f2966999
mdgeer <mdgeer25@gmail.com>
2024-08-11 16:34:57 +0200

    task 5.3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,44 @@
 function initCarousel() {
   // ваш код...
+  let leftButton = document.querySelector('.carousel__arrow_left');
+  let rightButton = document.querySelector('.carousel__arrow_right');
+  let carousel = document.querySelector('.carousel__inner');
+  let slideWidth = carousel.offsetWidth;
+
+  let currentSlide = 1;
+  let currentCarouselPosition = 0;
+  
+  hideButtonsOnBorders();
+
+  function carouselGoRight() {
+    let newCarouselPosition = currentCarouselPosition + slideWidth;
+    carousel.style.transform = `translateX(${newCarouselPosition}px)`;
+
+    currentCarouselPosition = newCarouselPosition;
+    currentSlide--;
+    hideButtonsOnBorders();
+  }
+
+  function carouselGoLeft() {
+    let newCarouselPosition = currentCarouselPosition - slideWidth;
+    carousel.style.transform = `translateX(${newCarouselPosition}px)`;
+
+    currentCarouselPosition = newCarouselPosition;
+    currentSlide++;
+    hideButtonsOnBorders();
+  }
+
+  function hideButtonsOnBorders() {
+    if(currentSlide == 1) {
+      leftButton.style.display = 'none';
+    } else if (currentSlide == 4) {
+      rightButton.style.display = 'none';
+    } else {
+      leftButton.style.display = '';
+      rightButton.style.display = '';
+    }
+  }
+
+  leftButton.onclick = carouselGoRight;
+  rightButton.onclick = carouselGoLeft;
 }
