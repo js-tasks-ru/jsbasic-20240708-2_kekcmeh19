4830dcf03413c2c428631579374ed3d4638cd53b
mdgeer <mdgeer25@gmail.com>
2024-08-11 13:31:54 +0200

    task 5.1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
   // ваш код...
+  let button = document.querySelector('.hide-self-button');
+  button.onclick = test;
+
+  function test() {
+    button.hidden = true;
+  }
 }
