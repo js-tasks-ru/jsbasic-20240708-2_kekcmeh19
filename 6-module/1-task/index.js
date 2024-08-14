4b5e4423e76ccbe5f4e03aee3600e9e8cfaf4e03
mdgeer <mdgeer25@gmail.com>
2024-08-14 17:16:27 +0200

    tasks 6.1, 6.2, 6.3

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,30 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.renderTable();
+  }
+
+  createTableHead() {
+    return (
+      `<th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th>`
+    )
+  }
+
+  createTableBody() {
+    this.rowsHTML = this.rows
+      .map(({ name, age, salary, city }) => {
+        return `<tr><td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button onclick="this.closest('tr').remove()">X</button></td></tr>`
+      })
+      .join('');
+    
+      return this.rowsHTML;
+  }
+
+  renderTable() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead><tr>${this.createTableHead()}</tr></thead><tbody>${this.createTableBody()}</tbody>`;
+
+    return this.elem;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,109 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.elem = this.render();
+  }
+
+  renderSlides() {
+    this.slidesHTML = this.slides
+      .map(({ name, price, image, id}) => {
+        return (`
+        <div class="carousel__slide" data-id="${id}">
+          <img src="/assets/images/carousel/${image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${price.toFixed(2)}</span>
+            <div class="carousel__title">${name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+        `);
+      })
+      .join('');
+
+      return this.slidesHTML;
+  }
+
+  renderButtons() {
+    return(`
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+      `)
+  }
+
+  createCarouselListeners() {
+    this.leftButton = this.elem.querySelector(".carousel__arrow_left");
+    this.rightButton = this.elem.querySelector(".carousel__arrow_right");
+    this.slides = this.elem.querySelectorAll('.carousel__slide');
+    this.carousel = this.elem.querySelector(".carousel__inner");
+
+    this.currentSlide = 0;
+
+    if (this.currentSlide == 0) {
+      this.leftButton.style.display = 'none';
+    }
+
+    this.leftButton.addEventListener('click', () => {
+      this.currentSlide--;
+      this.carousel.style.transform = `translateX(-${this.slides[0].offsetWidth * this.currentSlide}px)`
+      
+      if (this.currentSlide == 0) {
+        this.leftButton.style.display = 'none';
+      } else {
+        this.leftButton.style.display = '';
+        this.rightButton.style.display = '';
+      };
+    })
+
+    this.rightButton.addEventListener('click', () => {
+      this.currentSlide++;
+      this.carousel.style.transform = `translateX(-${this.slides[0].offsetWidth * this.currentSlide}px)`;
+      
+      if (this.currentSlide == this.slides.length - 1) {
+        this.rightButton.style.display = 'none';
+      } else {
+        this.leftButton.style.display = '';
+        this.rightButton.style.display = '';
+      };
+
+    })
+
+  }
+
+  createButtonListeners() {
+    this.buttons = this.elem.querySelectorAll('.carousel__button');
+    this.buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        let slideID = button.closest('.carousel__slide').dataset.id;
+
+        let event = new CustomEvent("product-add", {
+          detail: slideID,
+          bubbles: true
+        })
+
+        button.dispatchEvent(event);
+      })
+    })
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="carousel">
+        ${this.renderButtons()}
+        <div class="carousel__inner">
+        ${this.renderSlides()}
+        </div>
+      </div>
+    `);
+    
+    this.createCarouselListeners();
+    this.createButtonListeners();
+    
+    return this.elem;
   }
 }
